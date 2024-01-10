f5e2521501acee16108e679858878b5982c81d5b
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-01-10 01:57:19 -0800

    commit it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const header = document.querySelector("headr");
+const header = document.querySelector("header");
 
 window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",this.window.scrollY>0)
-})
+    header.classList.toggle("sticky",this.window.scrollY > 0)
+});
